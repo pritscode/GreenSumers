57fce5bf255c87aa1f4547d70eed7e5423b9facd
prit <156163564+pritscode@users.noreply.github.com>
2024-05-10 12:40:40 +0900

    Update MailVO.java

--- java/com/test/my/commons/vo/MailVO.java
+++ java/com/test/my/commons/vo/MailVO.java
@@ -6,35 +6,43 @@ public class MailVO {
 	
 	private String Title;
 	private String Content;
-	private String Giver;
+	private String Reciever;
 	private List<String> email;
 	public String getTitle() {
 		return Title;
 	}
-	public void setTitle(String title) {
-		Title = title;
-	}
-	public String getGiver() {
-		return Giver;
-	}
-	public void setGiver(String giver) {
-		Giver = giver;
-	}
+	
 	public String getContent() {
 		return Content;
 	}
+
 	public void setContent(String content) {
 		Content = content;
 	}
+
+	public String getReciever() {
+		return Reciever;
+	}
+
+	public void setReciever(String reciever) {
+		Reciever = reciever;
+	}
+
 	public List<String> getEmail() {
 		return email;
 	}
+
 	public void setEmail(List<String> email) {
 		this.email = email;
 	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
 	@Override
 	public String toString() {
-		return "MailVO [Title=" + Title + ", Content=" + Content + ", Giver=" + Giver + ", email=" + email + "]";
+		return "MailVO [Title=" + Title + ", Content=" + Content + ", Reciever=" + Reciever + ", email=" + email + "]";
 	}
 	
 }
