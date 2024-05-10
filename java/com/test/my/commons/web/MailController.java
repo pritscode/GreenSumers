4a195b0807bee6457722e20bc10775000253d215
prit <156163564+pritscode@users.noreply.github.com>
2024-05-10 12:44:22 +0900

    Update MailController.java

--- java/com/test/my/commons/web/MailController.java
+++ java/com/test/my/commons/web/MailController.java
@@ -38,7 +38,7 @@ public class MailController {
 		System.out.println(mailVO);
 		List<String> arr = mailVO.getEmail();
 		for(String email: arr) {
-			mailService.sendMail(mailVO.getGiver(), "GreenSumers 이메일 확인", "새로운 계정 생성 프로세스를 시작해 주셔서 감사합니다. 사용자가 본인임을 확인하려고 합니다. 메시지가 표시되면 다음 확인 코드를 입력하세요."+RandomStringGenerator.generateVerificationString()+"계정을 생성하지 않는 경우에는 이 메시지를 무시해도 됩니다.");
+			mailService.sendMail(mailVO.getReciever(), "GreenSumers 이메일 확인", "새로운 계정 생성 프로세스를 시작해 주셔서 감사합니다. 사용자가 본인임을 확인하려고 합니다. 메시지가 표시되면 다음 확인 코드를 입력하세요."+RandomStringGenerator.generateVerificationString()+"계정을 생성하지 않는 경우에는 이 메시지를 무시해도 됩니다.");
 		}
 		re.addFlashAttribute("msg", "메일이 전송되었습니다.");
 		return "redirect:/admin";
