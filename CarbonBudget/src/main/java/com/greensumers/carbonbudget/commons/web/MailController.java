49ae96452ebfd28067cda869460017aa3e3bef21
prit <156163564+pritscode@users.noreply.github.com>
2024-05-30 20:04:29 +0900

    Update MailController.java

--- CarbonBudget/src/main/java/com/greensumers/carbonbudget/commons/web/MailController.java
+++ CarbonBudget/src/main/java/com/greensumers/carbonbudget/commons/web/MailController.java
@@ -2,6 +2,8 @@ package com.greensumers.carbonbudget.commons.web;
 
 import java.io.UnsupportedEncodingException;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,10 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.greensumers.carbonbudget.commons.service.MailService;
-import com.greensumers.carbonbudget.commons.utils.RandomStringGenerator;
+import com.greensumers.carbonbudget.member.vo.MemberVO;
 
 @Controller
 @EnableAsync
@@ -20,20 +22,6 @@ public class MailController {
 	
 	@Autowired
 	private MailService mailService;
-	
-	@RequestMapping("/test")
-	public String test(RedirectAttributes re) throws UnsupportedEncodingException{
-		mailService.sendMail("pritscode@naver.com", "연습용 메일", RandomStringGenerator.generateVerificationString());
-		re.addFlashAttribute("msg", "성공적으로 메일이 전송되었습니다.");
-		return "redirect:/";
-	}
-	
-	@RequestMapping("/testVerification")
-	public String testVerification(RedirectAttributes re) throws UnsupportedEncodingException {
-		mailService.sendVerificationMail("pritscode@naver.com");
-		re.addFlashAttribute("msg", "성공적으로 메일이 전송되었습니다.");
-		return "redirect:/";
-	}
 	
 	@RequestMapping(value = "/sendVerifMailDo", method = { RequestMethod.POST })	
 	public ResponseEntity<String> test(@RequestParam("memEmail") String memEmail) {
@@ -46,15 +34,32 @@ public class MailController {
 		}
 	    return ResponseEntity.ok("Success"); // 예시로 "Success" 문자열 반환
 	}
+
+	/**
+	 * @Author : Pangsu
+	 * @Date   : 2024. 5. 13.
+	 * @Method : verifyCode
+	 * @return : String
+	 * Purpose : 메일 인증코드 체크 
+	 * Description : redis (remote dictionary server key-value 구조의 비정형 데이터 저장관리 도구)에 저장된 값 체크
+	*/
+	@RequestMapping(value = "/verify", method = RequestMethod.POST)
+    public @ResponseBody String verifyCode(@RequestParam String email, @RequestParam String code) {
+        boolean isVerified = mailService.verifyEmail(email, code);
+        return isVerified ? "Y" : "N";
+    }
 	
-	//@RequestMapping(value = "/VerificationDo", method = {RequestMethod.POST})
-	//public ResponseEntity<String>(){
-	//	System.out.println();
-	//	try {
-	//		mailService.CheckingCode();
-	//	} catch (UnsupportedEncodingException e) {
-	//		e.printStackTrace();
-	//	}
-	//	return ResponseEntity.ok("Success");
-	//}
+	@RequestMapping(value = "/sendTemporaryPasswordMailDo")
+	public ResponseEntity<String> temporaryPasswordMailTest(MemberVO vo, HttpSession session) {
+		MemberVO memberFindingPw = (MemberVO) session.getAttribute("memberFindingPw");
+		vo.setMemEmail(memberFindingPw.getMemEmail());
+		System.out.println(vo);
+		try {
+			mailService.sendTemporaryPasswordMail(vo.getMemEmail());
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		session.invalidate();
+		return ResponseEntity.ok("Success");
+	}
 }
