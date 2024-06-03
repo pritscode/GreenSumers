7d506cad82c9b7e671c386e7f312d6e623883a2b
prit <156163564+pritscode@users.noreply.github.com>
2024-06-03 12:40:26 +0900

    Update IComparisonDAO.java

--- CarbonBudget/src/main/java/com/greensumers/carbonbudget/comparison/dao/IComparisonDAO.java
+++ CarbonBudget/src/main/java/com/greensumers/carbonbudget/comparison/dao/IComparisonDAO.java
@@ -9,6 +9,9 @@ import com.greensumers.carbonbudget.comparison.vo.ComparisonVO;
 @Mapper
 public interface IComparisonDAO {
 
-	// 사용정보 불러오기
+	// 사용자 정보 불러오기
 	public List<ComparisonVO> checkInUsageData(ComparisonVO vo);
+	
+	// 전체 사용자 정보 불러오기
+	public List<ComparisonVO> userData(ComparisonVO vo);
 }
