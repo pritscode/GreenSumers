3067234551e3d92aab654ac102d415c8149db3dd
prit <156163564+pritscode@users.noreply.github.com>
2024-06-03 10:38:46 +0900

    Update ComparisonVO.java

--- CarbonBudget/src/main/java/com/greensumers/carbonbudget/comparison/vo/ComparisonVO.java
+++ CarbonBudget/src/main/java/com/greensumers/carbonbudget/comparison/vo/ComparisonVO.java
@@ -9,4 +9,12 @@ public class ComparisonVO {
 	private String gasUsage;
 	private String elctrUsage;
 	private String useYm;
+	
+	public String getGasUsage() {
+        return gasUsage != null ? gasUsage : "0.0";
+    }
+	
+	public String getElctrUsage() {
+        return elctrUsage != null ? elctrUsage : "0.0";
+    }
 }
